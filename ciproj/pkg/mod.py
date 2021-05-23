30c100d37a6b7f3cc6fa35adbddbd1534d106421
hezhiming <he.zhiming@foxmail.com>
2021-05-23 12:29:33 +0800

    FIX pylint

--- ciproj/pkg/mod.py
+++ ciproj/pkg/mod.py
@@ -9,6 +9,15 @@ class Calculator:
     class doc
     """
 
+    def __init__(self, num1, num2):
+        """constructor
+
+        :param num1:
+        :param num2:
+        """
+        self.num1 = num1
+        self.num2 = num2
+
     def add(self, num1: int, num2: int):
         """ add func
 
